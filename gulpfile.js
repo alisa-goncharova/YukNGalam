f0ca117ead0c868c57e389eabd9b80bf7ea0da0c
alice <alice.goncharova2018@yandex.by>
2020-04-22 14:52:32 +0300

    Подготовка нового проекта
    - создание директории, перенос директории на рабочий стол
    - инициализация проекта
    - создание базовой структуры проекта
    - установка плагинов
    - написание тасков (watch, sass и др)
    - измнение игнор-файла

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+// var minCSS = require("gulp-clean-css");
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+
+// конвертация sass в css
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/style.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/css/'));
+});
+// автоатическое сравнение sass
+gulp.task('default', function () {
+  gulp.watch('./src/sass/', gulp.series('sass', 'browser-sync'));
+});
+
+
+// использование плагина на перезагрузку страницы 
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./src/index.html"
+        }
+    });
+});
+
+
